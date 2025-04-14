33f718d42253a5da3871bf2cdfa8ca88ec3e52c0
snehasahu7 <153064528+snehasahu7@users.noreply.github.com>
2025-04-14 09:32:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://memtag-snehas-projects-ca79600d.vercel.app'
-}));
+app.use(cors());
 
 app.use(express.json());
 
