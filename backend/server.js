a667386b1ac6488e9c0a4ae4c574e7eb909725e6
snehasahu7 <153064528+snehasahu7@users.noreply.github.com>
2025-04-14 09:10:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
- 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://memtag-snehas-projects-ca79600d.vercel.app'
+}));
+
+app.use(express.json());
+
+
+app.get('/api/waitlist', (req, res) => {
+  res.json({ message: 'Waitlist route is working!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
